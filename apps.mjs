e724bbe150ccf058224bc8e19426f02d79883ae8
atizazalam <153724071+atizazalam@users.noreply.github.com>
2024-01-08 14:52:08 +0500

    Add files via upload

---
+++ apps.mjs
@@ -0,0 +1,10 @@
+let welcome ="Hello";
+let name ="Aitzaz";
+let age ="and your age is";
+let him ="23";
+console.log(welcome);
+console.log(name);
+console.log(age);
+console.log(him);
+document.querySelector(".sub").innerHTML=("Hello Aitzaz and your age is 23");
+
